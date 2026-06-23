bf455bb1e75e7f538e4f807ec64f83836b5115c1
username-22-ssh <shaktiofficial169@gmail.com>
2026-06-23 13:26:35 +0530

    Assignment completed

---
+++ Day 4/assignment.py
@@ -0,0 +1,40 @@
+'''
+1. Add comments to this code to explain what each line does:
+
+---
+name = "Alice"
+print("Hello", name)
+---
+
+2. Use escape sequences to format the following:
+
+---
+Output should be:
+Name:    John
+Age:     30
+He said, "I'm learning Python."
+---
+
+3. Print this using only one print statement:
+
+---
+Apple | Banana | Mango <End>
+---
+
+4. (Challenge) Write a multi-line message using \n and \t to format it like:
+
+---
+Welcome to Python!
+    Learn to code
+		Have fun!
+---
+
+'''
+name = "Alice"  # Assign the string "Alice" to the variable 'name'
+print("Hello", name) # Print the greeting "Hello" followed by the value of the variable 'name'.
+
+print("NAme:\tJohn\nAge:\t30\nHe said, \"I'm learning Python.\"")
+
+print("Apple","|","Banana","|","Mango","<End>")
+
+print("Welcome to Python!\n\tLearn to code\n\t\tHave fun!")
---
+++ Day 4/practice.py
@@ -0,0 +1,19 @@
+# print statements
+
+name = "Ramu"
+age = 30
+city = "Kolkata"
+height = 5.10
+
+
+print("Hello World!",end="\n")
+print(name, age, city, height, sep=" , ", end="\n")
+
+
+#escape sequences
+# print("This is line one data.","\n","This should appear in new line.")
+# print("This is line one data.","\t","This should appear in new line.")
+# print("This is line one data.\",\"\\t\"\,\"This should appear in new line.")
+print("My name is : ",name)
+# print("This is random info, please ignore")
+print(name,'is',age,'years old')
