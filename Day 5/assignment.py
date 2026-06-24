92686c367877b6eb1dcdb6fe5c8aeb1ce103685a
username-22-ssh <shaktiofficial169@gmail.com>
2026-06-25 02:13:57 +0530

    Day 5 code and assignment of to task 2 is completed

---
+++ Day 5/assignment.py
@@ -0,0 +1,30 @@
+# Task 1
+Name = "Sahu"
+Age = 67
+Favorite_Programming_Language = "C++ and Rust"
+Is_Student = False
+
+
+print(f"Hello everyone,my name is {Name}")
+print(f"I'm {Age} years old")
+print(f"I love to code in {Favorite_Programming_Language}")
+
+if Is_Student:
+    print("I am a student")
+else:
+    print("I am a professional programmer")
+    
+
+#Task 2
+
+x = 10
+y = 3.14
+z = x + y
+print(z)
+print(type(z))
+
+
+
+#Task 3
+
+
---
+++ Day 5/practice.py
@@ -0,0 +1,14 @@
+my_name = "Vikash Das"
+age = 25
+height = 5.10
+is_adult = True
+# print(type(my_name))
+# print(type(age))
+# print(type(height))
+# print(type(is_adult))
+# print(type("india"))
+# print(type(5.5)) 
+
+print("Hello ", my_name, ", nice to meet you!. You will turn 100 years old in next ",100-age,"yrs.")
+
+
