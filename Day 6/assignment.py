cd86cb9b7d75861b18e5873b8da5ad0656aad1bc
username-22-ssh <shaktiofficial169@gmail.com>
2026-06-25 13:04:40 +0530

    Day 6 practice and assignment completed

---
+++ Day 6/assignment.py
@@ -0,0 +1,35 @@
+#Task 1:
+name = input("Your name pls --> ")
+DOB = int(input("Enter your dob -->"))
+
+print(f"Hello {name}, you are {2026-DOB} years old! ")
+
+
+#Task 2:
+num1 = int(input("NEter your first number --> "))
+num2 = int(input("Eneter your second number -->"))
+
+print(f"Thier sum is {num1+num2}")
+print(f"Thier difference is {num1-num2}")
+print(f"Thier product is {num1*num2}")
+
+#Task 3:
+float_num = float(input("Enter a float number --> "))
+print(f"float value is {float_num} and integer value is {int(float_num)}")
+
+
+
+
+
+#Task 4:
+word1 = input("Enter your first word -->")
+word2 = input("Enter your second word -->")
+print(f"{word2}, {word1}")
+
+
+
+#Task 5:
+num = int(input("Enter a number --> "))
+num = num * 5
+num = str(num)
+print(f"Your number multiplied by 5 is: {num}")
---
+++ Day 6/practice.py
@@ -0,0 +1,7 @@
+print("<<<Hello, welcome to the practice session>>>")
+
+name = input("What is your name? ")
+age = int(input("What is your age? "))
+
+print("Hello", name,"it's nice to meet you. You are",age,"yrs old.")
+print(f"You were born in the year of {2025-age}" )
