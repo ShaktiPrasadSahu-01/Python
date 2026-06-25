59f38bbbffe99a1191d692bf1b9ba4df49f63c47
username-22-ssh <shaktiofficial169@gmail.com>
2026-06-25 11:11:59 +0530

    Day 5, all assignment completed

--- Day 5/assignment.py
+++ Day 5/assignment.py
@@ -27,4 +27,33 @@ print(type(z))
 
 #Task 3
 
+number = "123"
+if number.isdigit():
+    x = int(number)
+    print(f"The number is {x+10}")
+else:
+    print("The number is not a valid integer")
+    
+    
+    
+    
+#Task 4   
+# 2nmae= 30
+# print(2nmae)
+# my-name = "Sahu"
+# print(my-name)
 
+#Task 5
+''' Can a variable store multiple data types during execution? 
+Yes, in Python, a variable can store multiple data types 
+during execution. For example, you can assign an integer to 
+a variable and later assign a string or a list to the same 
+variable. This is because Python is a dynamically typed language, 
+which means that the type of a variable is determined at 
+runtime and can change as needed. '''
+
+x = 10 # Initially an integer
+print(type(x)) # Output: <class 'int'>
+
+x = "Hello" # Now a string
+print(type(x)) # Output: <class 'str'>
