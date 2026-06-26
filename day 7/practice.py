ed90b184361107c661a42d442bc52260de237df9
username-22-ssh <shaktiofficial169@gmail.com>
2026-06-26 12:30:36 +0530

    Day 7 completed

---
+++ day 7/practice.py
@@ -0,0 +1,30 @@
+#Arithmetic Operators
+a = 10
+b = 3
+
+print("Addition: ", a + b)
+print("Subtraction: ", a - b)
+print("Multiplication: ", a * b)
+print("Division: ", a / b)
+print("Floor Division: ", a // b)
+print("Modulus: ", a % b)
+print("Exponentiation: ", a ** b)
+
+#comparison Operators
+x = 5
+y = 10
+
+print("Equal: ", x == y)
+print("Not Equal: ", x != y)
+print("Greater than: ", x > y)
+print("Less than: ", x < y)
+print("Greater than or equal to: ", x >= y)
+print("Less than or equal to: ", x <= y)
+
+#logical Operators
+age = 28
+has_license = True
+
+print("Can drive: ", age >= 18 and has_license)
+print("Cannot drive: ", age < 18 or not has_license)
+print("Eligible for senior citizen benefits: ", age >= 60 and has_license)  
