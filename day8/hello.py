6e6681071f0d5eda190a0a6e3869f2904bd772eb
username-22-ssh <shaktiofficial169@gmail.com>
2026-07-11 01:10:15 +0530

    done with string basics

---
+++ day8/hello.py
@@ -0,0 +1,4 @@
+s = " sh "
+print(id(s))
+s = s.strip()
+print(id(s))
---
+++ day8/practice.py
@@ -0,0 +1,17 @@
+message = "  Learn Python!  "
+
+
+
+print(message.strip())  # Removes leading and trailing whitespace
+print(message.lstrip())  # Removes leading whitespace
+print(message.rstrip())  # Removes trailing whitespace
+print(message.upper())  # Converts to uppercase
+print(message.lower())  # Converts to lowercase
+print(message.split(" "))
+print(message.replace("Python", "Coding").strip())  # Replaces "Python" with "Coding" and removes whitespace
+message=message.replace("Python", "Coding").strip()  # Replaces "Python" with "Coding" and removes whitespace
+
+
+
+print(message.find("P"))
+print(message.split("!"))
